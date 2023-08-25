a8bf74a22562b462e566851b60ff4dbcbc5bd14c
abhay2002-pro <abhayray2002@gmail.com>
2023-08-25 23:57:15 +0530

    Added reverse proxy

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const rateLimit = require("express-rate-limit");
 
 const { ServerConfig } = require("./config");
 const apiRoutes = require("./routes");
+const { createProxyMiddleware } = require("http-proxy-middleware");
 
 const app = express();
 
@@ -16,6 +17,8 @@ app.use(express.urlencoded({extended: true}));
 
 app.use(limiter);
 
+app.use('/flightservice', createProxyMiddleware({ target: ServerConfig.FLIGHT_SERVICE, changeOrigin: true, pathRewrite: {'^/flightservice' : '/'} }));
+app.use('/bookingservice', createProxyMiddleware({ target: ServerConfig.BOOKING_SERVICE, changeOrigin: true, pathRewrite: {'^/bookingservice' : '/'} }));
 app.use("/api", apiRoutes);
 
 app.listen(ServerConfig.PORT, () => {
